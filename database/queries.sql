c474b4f2d326ce90d1323d5f639a2733189f6b65
Mindsky <romain.rondet@gmail.com>
2021-04-13 19:48:30 +0200

    Update queries

---
+++ database/queries.sql
@@ -0,0 +1,15 @@
+-- Select the address and informations of all the restaurants.
+
+-- Select the informations and address of all the users per restaurant.
+
+-- Select the informations and the content of a command.
+
+-- Select the commands of a restaurant.
+
+-- Select all the ingredients that are out of stock in the restaurants.
+
+-- Select all the products that can't be made or sold in the restaurants.
+
+-- Select a product recipe.
+
+-- Select the restaurant making the most revenue.
